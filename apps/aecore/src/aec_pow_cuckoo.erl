30196369e3de982a046049d2f8b17d5003a11b2a
Akos Korosmezey <akos.korosmezey@erlang-solutions.com>
2017-11-09 15:13:36 +0100

    Paths corrected

--- apps/aecore/src/aec_pow_cuckoo.erl
+++ apps/aecore/src/aec_pow_cuckoo.erl
@@ -11,8 +11,8 @@
 
 -behaviour(aec_pow).
 
+
 -export([generate/3,
-         generate/5,
          verify/4]).
 
 
@@ -21,30 +21,28 @@
 -include_lib("eunit/include/eunit.hrl").
 -endif.
 
--define(CUCKOO_ALGORITHM, mean).
--define(CUCKOO_GRAPH_SIZE, 30).
--define(SERVER, ?MODULE).
+-define(DEFAULT_MINER_EXECUTABLE, mean30).
+-define(DEFAULT_NODE_BITS, 30).
+-define(DEFAULT_EXTRA_OPTS, "-t 5"). %% -m 7 caused crash
 
 -ifdef(TEST).
 -define(debug(F, A), ok).
 -define(info(F, A), ?debugFmt(F, A)).
 -define(error(F, A), ?debugFmt(F, A)).
 -else.
--define(NIF_NAME, "aec_pow_cuckoo28_nif").
+-define(debug(F, A), epoch_pow_cuckoo:debug(F, A)).
+-define(info(F, A),  epoch_pow_cuckoo:info(F, A)).
+-define(error(F, A), epoch_pow_cuckoo:error(F, A)).
 -endif.
 
-
--on_load(init/0).
+-record(state, {os_pid :: integer() | undefined,
+                buffer = [] :: string(),
+                target :: aec_pow:sci_int() | undefined,
+                parser :: output_parser_fun()}).
 
 -type pow_cuckoo_solution() :: [integer()].
-
-
-%%%=============================================================================
-%%% NIF initialization
-%%%=============================================================================
-
-init() ->
-    ok = erlang:load_nif(filename:join([code:priv_dir(aecore), ?NIF_NAME]), 0).
+-type output_parser_fun() :: fun((list(string()), #state{}) ->
+                                      {'ok', term()} | {'error', term()}).
 
 %%%=============================================================================
 %%% API
@@ -70,18 +68,9 @@ init() ->
 -spec generate(Data :: aec_sha256:hashable(), Target :: aec_pow:sci_int(),
                Nonce :: integer()) -> aec_pow:pow_result().
 generate(Data, Target, Nonce) ->
-    generate(Data, Nonce, Target, 7, 5).
-
-%%------------------------------------------------------------------------------
-%% Proof of Work generation, all params adjustable
-%%------------------------------------------------------------------------------
--spec generate(Data :: aec_sha256:hashable(), Nonce :: integer(),
-               Target :: aec_pow:sci_int(), Trims :: integer(),
-               Threads :: integer()) ->
-                      aec_pow:pow_result().
-generate(Data, Nonce, Target, Trims, Threads) ->
+    %% Hash Data and convert the resulting binary to a base64 string for Cuckoo
     Hash = base64:encode_to_string(aec_sha256:hash(Data)),
-    generate_hashed(Hash, Nonce, Target, Trims, Threads).
+    generate_int(Hash, Nonce, Target).
 
 %%------------------------------------------------------------------------------
 %% Proof of Work verification (with difficulty check)
@@ -105,44 +94,253 @@ verify(Data, Nonce, Evd, Target) when is_list(Evd) ->
 %%------------------------------------------------------------------------------
 %% Proof of Work generation: use the hash provided
 %%------------------------------------------------------------------------------
-generate_hashed(Hash, Nonce, Target, Trims, Threads) ->
-    Nonce32 = Nonce band 16#7fffffff,
-    case generate_single(Hash, Nonce32, Trims, Threads) of
-        {error, no_solutions} ->
-            {error, no_solution};
+-spec generate_int(Hash :: string(), Nonce :: integer(), Target :: aec_pow:sci_int()) ->
+                          {'ok', Nonce2 :: integer(), Solution :: pow_cuckoo_solution()} |
+                          {'error', term()}.
+generate_int(Hash, Nonce, Target) ->
+    case generate_single(Hash, Nonce, Target) of
         {ok, Soln} ->
-            case test_target(Soln, Target) of
-                true ->
-                    {ok, {Nonce32, Soln}};
-                false ->
-                    {error, no_solution}
-            end
+            {ok, {Nonce, Soln}};
+        {error, no_value} ->
+            ?debug("No cuckoo solution found~n", []),
+            {error, no_solution};
+        {error, _Reason} = Error ->
+            %% Executable failed (segfault, not found, etc.): let miner decide
+            Error
     end.
 
 %%------------------------------------------------------------------------------
-%% Proof of Work generation, a single attempt
+%% @doc
+%%   Proof of Work generation, a single attempt
+%% @end
 %%------------------------------------------------------------------------------
--spec generate_single(Header :: string(), Nonce :: integer(), Trims :: integer(),
-                      Threads :: integer()) ->
+-spec generate_single(Hash :: string(), Nonce :: integer(), Target :: aec_pow:sci_int()) ->
                              {'ok', Solution :: pow_cuckoo_solution()} |
-                             {'error', 'no_solutions'}.
-generate_single(_Header, _Nonce, _Trims, _Threads) ->
-    erlang:nif_error(nif_library_not_loaded).
+                             {'error', term()}.
+generate_single(Hash, Nonce, Target) ->
+    PrivDir = code:priv_dir(ae_cuckoo_cycle_pow_executables),
+    BinDir = filename:join([PrivDir, "bin"]),
+    LibDir = filename:join([PrivDir, "lib"]),
+    ensure_exec_started(),
+    Exe = application:get_env(aecore, cuckoo_miner_executable, ?DEFAULT_MINER_EXECUTABLE),
+    Extra = application:get_env(aecore, cuckoo_extra_options, ?DEFAULT_EXTRA_OPTS),
+    LdPathVar = case os:type() of
+                 {unix, darwin} -> "DYLD_LIBRARY_PATH";
+                 {unix, _}      -> "LD_LIBRARY_PATH"
+                end,
+    %% -s makes mean miner print out the solution. We do not make it
+    %% Algo-dependent in order to avoid wiring-in executable names. Lean miner
+    %% will complain but ignores it.
+    ?info("Executing cmd: ~p~n", [lists:concat(["./", Exe, " -h ", Hash, " -n ", Nonce, " -s ", Extra])]),
+    try exec:run(
+          lists:concat(["export ", LdPathVar, "=", LibDir, "; ./", Exe, " -h ", Hash, " -n ", Nonce, " -s ", Extra]),
+          [{stdout, self()},
+           {stderr, self()},
+           {kill_timeout, 1},
+           {cd, BinDir},
+           {env, [{"SHELL", "/bin/sh"},
+                  {LdPathVar, LibDir}]},
+           monitor]) of
+        {ok, _ErlPid, OsPid} ->
+            wait_for_result(#state{os_pid = OsPid,
+                                   buffer = [],
+                                   parser = fun parse_generation_result/2,
+                                   target = Target})
+    catch
+            C:E ->
+            {error, {C, E}}
+    end.
 
 %%------------------------------------------------------------------------------
-%% Proof of Work verification (without difficulty check)
+%% @doc
+%%   Proof of Work verification (without difficulty check)
+%% @end
 %%------------------------------------------------------------------------------
 -spec verify(Hash :: string(), Nonce :: integer(),
-             Soln :: aec_pow:pow_evidence()) -> boolean().
-verify(_Hash, _Nonce, _Soln) ->
-    erlang:nif_error(nif_library_not_loaded).
+             Soln :: aec_pow:pow_evidence()) -> boolean() | {error, term}.
+verify(Hash, Nonce, Soln) ->
+    PrivDir = code:priv_dir(ae_cuckoo_cycle_pow_executables),
+    BinDir = filename:join([PrivDir, "bin"]),
+    LibDir = filename:join([PrivDir, "lib"]),
+    ensure_exec_started(),
+    LdPathVar = case os:type() of
+                 {unix, darwin} -> "DYLD_LIBRARY_PATH";
+                 {unix, _}      -> "LD_LIBRARY_PATH"
+                end,
+    Size = application:get_env(aecore, cuckoo_node_bits, ?DEFAULT_NODE_BITS),
+    SolnStr = solution_to_hex(Soln),
+    ?info("Executing: ~p~n", [lists:concat(["export ", LdPathVar, "=", LibDir, "; ./verify", Size, " -h ", Hash, " -n ", Nonce])]),
+    try exec:run(
+              lists:concat(["export ", LdPathVar, "=", LibDir, "; ./verify", Size, " -h ", Hash, " -n ", Nonce]),
+              [{stdout, self()},
+               {stderr, self()},
+               stdin,
+               {kill_timeout, 1}, %% Kills exe with SIGTERM, then with SIGKILL if needed
+               {cd, BinDir},
+               {env, [{"SHELL", "/bin/sh"},
+                      {LdPathVar, LibDir}]},
+               monitor]) of
+        {ok, _ErlPid, OsPid} ->
+            exec:send(OsPid, list_to_binary(SolnStr)),
+            exec:send(OsPid, eof),
+            ParserState = #state{os_pid = OsPid,
+                                        buffer = [],
+                                        parser = fun parse_verification_result/2},
+            case wait_for_result(ParserState) of
+                {ok, Value} ->
+                    Value;
+                Err ->
+                    Err
+            end
+    catch
+            C:E ->
+            {error, {C, E}}
+    end.
 
 %%------------------------------------------------------------------------------
-%% Fetch the size of solution elements
+%% @doc
+%%   Receive and process notifications about the fate of the process and its
+%%   output. The receieved stdout tends to be in large chunks, we keep a buffer
+%%   for the last line fragment w/o NL.
+%% @end
+%%------------------------------------------------------------------------------
+-spec wait_for_result(#state{}) ->
+                             {'ok', Solution :: pow_cuckoo_solution()} |
+                             {'error', term()}.
+wait_for_result(#state{os_pid = OsPid,
+                      buffer = Buffer} = State) ->
+    receive
+        {stdout, OsPid, Msg} ->
+            Str = binary_to_list(Msg),
+            {Lines, NewBuffer} = handle_fragmented_lines(Str, Buffer),
+            (State#state.parser)(Lines, State#state{buffer = NewBuffer});
+        {stderr, OsPid, Msg} ->
+            ?error("ERROR: ~s~n", [Msg]),
+            wait_for_result(State);
+        {'DOWN', OsPid, process, _, normal} ->
+            %% Process ended but no value found
+            {error, no_value};
+        {'DOWN', OsPid, process, _, Reason} ->
+            %% Abnormal termination
+            Reason2 = case Reason of
+                          {exit_status, ExStat} -> exec:status(ExStat);
+                          _                     -> Reason
+                      end,
+            ?error("OS process died: ~p~n", [Reason2]),
+            {error, {execution_failed, Reason2}};
+        Other ->
+            ?debug("Ignoring message ~p~n", [Other]),
+            wait_for_result(State)
+    end.
+
+%%------------------------------------------------------------------------------
+%% @doc
+%%   Prepend the first new incoming line with the last line fragment stored
+%%   in Buffer and replace Buffer with the possible new line fragment at the
+%%   end of Str.
+%% @end
+%%------------------------------------------------------------------------------
+-spec handle_fragmented_lines(string(), string()) -> {list(string()), string()}.
+handle_fragmented_lines(Str, Buffer) ->
+    Lines = string:tokens(Str, "\n"),
+
+    %% Add previous truncated line if present to first line
+    Lines2 =
+        case Buffer of
+            [] ->
+                Lines;
+            _ ->
+                [Line1 | More] = Lines,
+                [Buffer ++ Line1 | More]
+        end,
+
+    %% Keep last fraction (w/o NL) in buffer
+    case lists:last(Str) of
+        $\n ->
+            {Lines2, ""};
+        _ ->
+            {L3, [Bf]} = lists:split(length(Lines) - 1, Lines2),
+            {L3, Bf}
+    end.
+
+%%------------------------------------------------------------------------------
+%% @doc
+%%   Parse miner output
+%% @end
+%%------------------------------------------------------------------------------
+-spec parse_generation_result(list(string()), #state{}) ->
+                                     {'ok', Solution :: pow_cuckoo_solution()} |
+                                     {'error', term()}.
+parse_generation_result([], State) ->
+    wait_for_result(State);
+parse_generation_result(["Solution" ++ ValuesStr | Rest], #state{os_pid = OsPid,
+                                                                 target = Target} = State) ->
+    Soln = [list_to_integer(V, 16) || V <- string:tokens(ValuesStr, " ")],
+    case test_target(Soln, Target) of
+        true ->
+            ?debug("Solution found: ~p~n", [Soln]),
+            stop_execution(OsPid),
+            {ok, Soln};
+        false ->
+            %% failed to meet target: go on, we may find another solution
+            ?debug("Failed to meet target~n", []),
+            parse_generation_result(Rest, State)
+    end;
+parse_generation_result([Msg | T], State) ->
+    ?debug("~s~n", [Msg]),
+    parse_generation_result(T, State).
+
+%%------------------------------------------------------------------------------
+%% @doc
+%%   Parse verifyer output
+%% @end
+%%------------------------------------------------------------------------------
+-spec parse_verification_result(list(string()), #state{}) ->
+                                       {'ok', Solution :: pow_cuckoo_solution()} |
+                                       {'error', term()}.
+parse_verification_result([], State) ->
+    wait_for_result(State);
+parse_verification_result(["Verified with cyclehash" ++ _ | _Rest], #state{os_pid = OsPid}) ->
+    ?debug("PoW verified.~n", []),
+    stop_execution(OsPid),
+    {ok, true};
+parse_verification_result(["FAILED due to" ++ Reason | _Rest], #state{os_pid = OsPid}) ->
+    ?error("PoW verification failed: ~s~n", [Reason]),
+    stop_execution(OsPid),
+    {ok, false};
+parse_verification_result([Msg | T], State) ->
+    ?debug("~s~n", [Msg]),
+    parse_verification_result(T, State).
+
+%%------------------------------------------------------------------------------
+%% @doc
+%%   Ask erlexec to stop the OS process
+%% @end
+%%------------------------------------------------------------------------------
+-spec stop_execution(integer()) -> ok.
+stop_execution(OsPid) ->
+    case exec:stop(OsPid) of
+        {error, Reason} ->
+            ?debug("Failed to stop mining OS process ~p: ~p (may have already finished).~n",
+                   [OsPid, Reason]);
+        R ->
+            ?debug("Mining OS process ~p stopped successfully: ~p~n",
+                   [OsPid, R])
+    end.
+
+%%------------------------------------------------------------------------------
+%% @doc
+%%   The Cuckoo solution is a list of uint32 integers unless the graph size is
+%%   greater than 33 (when it needs u64 to store). Hash result for difficulty
+%%   control accordingly.
+%% @end
 %%------------------------------------------------------------------------------
 -spec get_node_size() -> integer().
 get_node_size() ->
-    erlang:nif_error(nif_library_not_loaded).
+    case application:get_env(aecore, cuckoo_node_bits, ?DEFAULT_NODE_BITS) of
+        L when L > 33 -> 8;
+        _ -> 4
+    end.
 
 %%------------------------------------------------------------------------------
 %% White paper, section 9: rather than adjusting the nodes/edges ratio, a
@@ -168,3 +366,23 @@ solution_to_binary([], _Bits, Acc) ->
     Acc;
 solution_to_binary([H | T], Bits, Acc) ->
     solution_to_binary(T, Bits, <<Acc/binary, H:Bits>>).
+
+ensure_exec_started() ->
+    case exec:start([]) of
+        {ok, _} ->
+            ok;
+        {error, {already_started, _}} ->
+            ok;
+        {error, _} = Error ->
+            Error
+    end.
+
+-spec solution_to_hex(list(integer())) -> string().
+solution_to_hex(Sol) ->
+    solution_to_hex(Sol, []).
+
+-spec solution_to_hex(list(integer()), list(string())) -> string().
+solution_to_hex([], Acc) ->
+    string:join(lists:reverse(Acc), " ");
+solution_to_hex([H | T], Acc) when is_integer(H) ->
+    solution_to_hex(T, [lists:flatten(io_lib:format("~8.16.0b", [H])) | Acc]).
